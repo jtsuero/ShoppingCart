6dfdd1c19e8fff83f46db591976d328a4a8da917
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2021-06-01 19:36:06 -0700

    test cart total amount for accuracy

--- test.js
+++ test.js
@@ -4,8 +4,16 @@ const cart = new ShoppingCart();
 const assert = require('chai').assert;
 
 describe('ShoppingCart', function () {
-  it('on initialization, shopping cart total and item list should be empty', function () {
-    let amountResult = cart.totalAmount();
-    assert.equal(amountResult, 0);
+  //add items to cart, create a duplicate of newspaper
+  cart.add('STAPOT');
+  cart.add('STARIC');
+  cart.add('STACOF');
+  cart.add('MEDNEW');
+  cart.add('MEDNEW');
+  let amountResult = cart.totalAmount();
+
+  it('cart total should equal 60.97', function () {
+    assert.equal(amountResult, 60.97);
+    assert.typeOf(amountResult, 'number');
   });
 });
