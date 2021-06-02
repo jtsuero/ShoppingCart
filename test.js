2912cf9400bec77533afc4d5b08ce763ced00926
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2021-06-01 19:36:35 -0700

    test cart item list has no duplicates

--- test.js
+++ test.js
@@ -11,9 +11,16 @@ describe('ShoppingCart', function () {
   cart.add('MEDNEW');
   cart.add('MEDNEW');
   let amountResult = cart.totalAmount();
+  let itemList = cart.itemsList();
 
   it('cart total should equal 60.97', function () {
     assert.equal(amountResult, 60.97);
     assert.typeOf(amountResult, 'number');
+  });
+
+  it('after adding items to cart, price and cart items update accordingly', function () {
+    assert.equal(itemList.length, 4);
+    //ensure no duplicates
+    assert.deepEqual(itemList, ['Coffee', 'Newspaper', 'Potatoes', 'Rice']);
   });
 });
